68681af93c47059375714d64747b11f885de5398
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-06-21 18:59:36 -0700

    Change Card to ProductCard—

---
+++ client/src/components/ProductCard.jsx
@@ -0,0 +1,27 @@
+import "../styles/ProductCard.css";
+
+function ProductCard(props) {
+  const title = props.title;
+  const description = props.description;
+  const price = props.price;
+  const foodImage = props.foodImage;
+  const foodImageAlt = props.foodImageAlt;
+
+
+  return (
+    <div className="ProductCard">
+      <div className="ProductCard-content">
+        <div className="ProductCard-item">
+          <h3 className="ProductCard-title">{title}</h3>
+          <p className="ProductCard-description">{description}</p>
+          <p className="ProductCard-price">{price}</p>
+        </div>
+        <div className="ProductCard-image">
+          <img src={foodImage} alt={foodImageAlt} height="96" width="96" />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProductCard;
