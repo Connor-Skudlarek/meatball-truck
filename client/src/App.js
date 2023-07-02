0da45d05e482485094ffc4482fa7d29ae1e25a8c
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-07-02 13:12:54 -0700

    Convert App.js to React Router

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,19 @@
-import "./App.css";
-import ProductWrapper from "./components/ProductWrapper";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./pages/LandingPage.jsx";
+import MenuPage from "./pages/MenuPage.jsx";
+// import AllergensPage from './AllergensPage.jsx';
+import "./styles/App.css";
+
 function App() {
   return (
-    <div className="App">
-      <p>Appetizers & Salads</p>
-      <ProductWrapper />
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/menu" element={<MenuPage />} />
+        {/* <Route path="/allergens" element={<AllergensPage />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
