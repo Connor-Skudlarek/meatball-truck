8a71b33db868b7512022d9df29f8e5dcad143915
Connor-Skudlarek <connor.skudlarek@gmail.com>
2023-07-02 13:11:20 -0700

    Create LandingPage component, moving from App.js

---
+++ client/src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import "../styles/LandingPage.css";
+import ProductWrapper from "../components/ProductWrapper";
+function LandingPage() {
+  return (
+    <div className="LandingPage">
+      <p>Appetizers & Salads</p>
+      <ProductWrapper />
+    </div>
+  );
+}
+
+export default LandingPage;
