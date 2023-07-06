f190a38daaaec021c5ae021cc987a5f710c8eb3a
Connor Skudlarek <connor.skudlarek@gmail.com>
2023-07-05 19:53:13 -0700

    Change var into const imports, add new-product route

--- server/app.js
+++ server/app.js
@@ -1,14 +1,15 @@
-require('dotenv').config()
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var mongoose = require("mongoose")
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-var indexRouter = require("./routes/index");
-var menuRouter = require("./routes/api/menu");
+const indexRouter = require("./routes/index");
+const menuRouter = require("./routes/api/menu");
+const productsRouter = require("./routes/api/products");
 
-var app = express();
+const app = express();
 
 app.use(logger("dev"));
 app.use(express.json());
@@ -16,14 +17,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "..", "client", "build")));
 
-mongoose.connect(process.env.MONGODB_URI, {useNewUrlParser: true, useUnifiedTopology: true});
-mongoose.connection.on('connected', () => {
-  console.log('Mongoose connected to MongoDB Atlas');
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on("connected", () => {
+  console.log("Mongoose connected to MongoDB Atlas");
 });
 
 app.use("/api/menu", menuRouter);
+app.use("/api/products", productsRouter);
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname,"..", "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "..", "client", "build", "index.html"));
 });
 app.use("/", indexRouter);
 
